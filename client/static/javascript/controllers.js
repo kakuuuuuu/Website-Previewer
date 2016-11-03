82167ff1dfd985c4453a7130a4bf883ff2757485
Kyle Tsuyemura <kt.fremont@gmail.com>
2016-11-02 20:43:42 -0700

    added close button

--- client/static/javascript/controllers.js
+++ client/static/javascript/controllers.js
@@ -19,7 +19,9 @@ preview_module.controller('previewController', function($scope, $http, previewFa
 
     scrape($scope.message)
   }
-
+  $scope.close = function(){
+    $scope.found= false;
+  }
   //Returns url with http:// added to input if not already there to comply with request call parameters
 
   function addhttp(url) {
@@ -36,7 +38,7 @@ preview_module.controller('previewController', function($scope, $http, previewFa
     //Regular Expression to check against input string for valid URL
 
     // var urlRegex = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi
-    var urlRegex= /(\b(?:(?:https?|ftp|file|[A-Za-z]+):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$]))/i
+    var urlRegex= /((?:(?:https?|ftp|file|[A-Za-z]+):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$]))/i
 
 
     //Checks if any word within string matches a valid URL
